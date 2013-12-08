1e5686bb9cc34cf00fc0da5312bc5a45fb4dad29
skewrain <hua@skewraindeMac-mini.local>
2013-12-08 19:47:00 +0800

    Initial Commit

---
+++ ToDoListOne/ABCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABCAppDelegate.h
+//  ToDoListOne
+//
+//  Created by skewrain on 13-12-8.
+//  Copyright (c) 2013年 skewrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoListOne/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoListOne
+//
+//  Created by skewrain on 13-12-8.
+//  Copyright (c) 2013年 skewrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABCAppDelegate class]));
+    }
+}
